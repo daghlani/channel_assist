c7836549fa7928a6b6f8c43018776f4a05d040a9
alid1371 <daghlani@elenoon.ir>
2019-04-30 02:13:57 +0430

    texts Files

---
+++ texts/filters.py
@@ -0,0 +1,12 @@
+start = ["start", "/start", "START", "Start", "/Start"]
+admin_menu = 'admin_menu'
+user_menu = 'user_menu'
+menu = 'menu'
+
+continue_btn = 'continue'
+sales_report_btn = 'sales_report'
+insert_product_btn = 'insert_product'
+state_view_btn = 'state_view_btn'
+cancel_btn = 'cancel'
+buy_btn = 'buy'
+
