e3d9a45888ee1f098f0740d65392375926f7a2d0
alid1371 <daghlani@elenoon.ir>
2019-05-01 15:43:51 +0430

    postgres config changed.

--- config/database.py
+++ config/database.py
@@ -1,10 +1,10 @@
 import os
 
-db_user = os.getenv('POSTGRES_USER', "")
-db_password = os.getenv('POSTGRES_PASSWORD', "")
-db_host = os.getenv('POSTGRES_HOST', "")
-db_name = os.getenv('POSTGRES_DB', "")
-db_port = os.getenv('POSTGRES_PORT', "")
+db_user = os.getenv('POSTGRES_USER', "postgres")
+db_password = os.getenv('POSTGRES_PASSWORD', "pass")
+db_host = os.getenv('POSTGRES_HOST', "localhost")
+db_name = os.getenv('POSTGRES_DB', "channel_assist")
+db_port = os.getenv('POSTGRES_PORT', "5432")
 # DB address
 dbadress = "postgresql://{}:{}@{}:{}/{}".format(db_user, db_password, db_host, db_port, db_name) or None
 # table names
