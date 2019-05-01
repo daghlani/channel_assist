aa8f12b95d39522c9d9af9464a7f647d1ea6b6d0
alid1371 <daghlani@elenoon.ir>
2019-05-01 15:32:27 +0430

    add rules and bio commands to bot

--- texts/filters.py
+++ texts/filters.py
@@ -4,6 +4,8 @@ user_menu = 'user_menu'
 menu = 'menu'
 
 continue_btn = 'continue'
+ruls_btn = 'ruls_btn'
+bio_btn = 'bio_btn'
 sales_report_btn = 'sales_report'
 insert_product_btn = 'insert_product'
 state_view_btn = 'state_view_btn'
